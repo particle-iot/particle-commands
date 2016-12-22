fb045f1504c21a70eef914a92757fbf2762378b0
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-12-22 15:00:33 +0100

    fix project.ino

--- src/cmd/templates/project/project.ino
+++ src/cmd/templates/project/project.ino
@@ -6,13 +6,13 @@
  */
 
 // setup() runs once, when the device is first turned on.
-setup() {
+void setup() {
   // Put initialization like pinMode and begin functions here.
 
 }
 
 // loop() runs over and over again, as quickly as it can execute.
-loop() {
+void loop() {
   // The core of your code will likely live here.
 
 }
