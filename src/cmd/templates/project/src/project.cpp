d70aefba5bd7401d118c52af5caed26efc4bf40f
keeramis <keerthy.amisagadda@gmail.com>
2023-10-10 14:28:49 -0500

    Update project template in the sources

--- src/cmd/templates/project/src/project.cpp
+++ src/cmd/templates/project/src/project.cpp
@@ -1,9 +1,9 @@
 /* 
- * Project <%=name%>
+ * Project myProject
  * Author: Your Name
  * Date: 
  * For comprehensive documentation and examples, please visit:
- * https://docs.particle.io/ (TODO: Replace with docs template link)
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
  */
 
 // Include Particle Device OS APIs
@@ -28,13 +28,8 @@ void setup() {
 void loop() {
   // The core of your code will likely live here.
 
-  // Example: Publish event to cloud every 10 seconds
-  /*
-  static system_tick_t start = millis();
-  if (Particle.connected() && millis() - start > 10000) {
-    Log.info("Publishing event to cloud...");
-		Particle.publish("Hello world!");
-	  start = millis();
-  }
-  */
+  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
+  // Logging.info("Sending Hello World to the cloud!");
+  // Particle.publish("Hello world!");
+  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
 }
