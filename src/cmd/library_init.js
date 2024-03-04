164554162dec7952775fb95ee7b5213869b45173
Hugo Montero <hugo.montero.ventura@particle.io>
2024-03-04 08:35:39 -0600

    implement the new library manager init process

--- src/cmd/library_init.js
+++ src/cmd/library_init.js
@@ -1,7 +1,5 @@
 import { Command, CommandSite } from './command';
-import { buildLibraryInitGeneratorClass } from 'particle-library-manager';
-
-const promisify = require('es6-promisify');
+import { LibraryInitGenerator } from 'particle-library-manager';
 
 /**
  * Specification and base implementation for the site instance expected by
@@ -13,20 +11,16 @@ export class LibraryInitCommandSite extends CommandSite {
 		super();
 	}
 
-	yeomanAdapter() {
-		throw new Error('not implemented');
-	}
-
-	yeomanEnvironment() {
-		throw new Error('not implemented');
-	}
-
 	options() {
 		return {};
 	}
 
 	args() {
 		return [];
+	}
+
+	prompter() {
+		throw new Error('not implemented');
 	}
 
 }
@@ -36,10 +30,6 @@ export class LibraryInitCommandSite extends CommandSite {
  */
 export class LibraryInitCommand extends Command {
 
-	generatorClass() {
-		return buildLibraryInitGeneratorClass();
-	}
-
 	/**
 	 *
 	 * @param {object} state The current conversation state.
@@ -47,23 +37,10 @@ export class LibraryInitCommand extends Command {
 	 * @returns {Promise} To run the library initialization command.
 	 */
 	run(state, site) {
-		const yeoman = site.yeomanEnvironment();
-		const args = site.args();
 		const opts = site.options();
-		const env = yeoman.createEnv(args, opts, site.yeomanAdapter());
-		env.registerStub(this.generatorClass(), 'library:init');
-		const run = promisify((...args) => env.run(...args));
-		return run(['library:init'], opts);
 
-		// we ideally want the instance so that we can hook the 'end' event
-		// yeoman-evironment doesn't provide the instance with the registration method above
-		// so we dive below the covers and do it ourselves...
-		// const generator = env.instantiate(LibraryInitGenerator, opts);
-		// return new Promise((fulfill, reject) => {
-		// 	generator.on('end', fulfill);
-		// 	generator.on('error', reject);
-		// 	generator.run();
-		// });
+		const generator = new LibraryInitGenerator({ prompt: site.prompter() });
+		generator.run({ options: opts });
 	}
 }
 
